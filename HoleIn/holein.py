1619ebe15cd77c9c852e0752355c57607ce8627d
HeeNamgoong <skrndgml2@kookmin.ac.kr>
2023-09-26 14:56:57 +0900

    Feat: Organize code clearly

--- HoleIn/holein.py
+++ HoleIn/holein.py
@@ -3,7 +3,8 @@
 import cv2
 import numpy as np
 
-def detect_hole_in_golf_ball(frame):
+
+def detect_hole_in(frame):
     
     # 노란색 홀컵 검출
     yellow_lower = np.array([20, 20, 100])
@@ -11,55 +12,52 @@ def detect_hole_in_golf_ball(frame):
     
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     yellow_mask = cv2.inRange(hsv_frame, yellow_lower, yellow_upper)
-    
     yellow_objects = cv2.bitwise_and(frame, frame, mask=yellow_mask)
     
-    cv2.imshow('holeCup', yellow_mask)
+    #cv2.imshow('HoleCup', yellow_mask)
     
     blurred_frame = cv2.GaussianBlur(yellow_objects, (5, 5), 0)
     gray_frame = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2GRAY)
     
     _, binary_frame = cv2.threshold(gray_frame, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     
-    # 침식과 팽창을 사용하여 이미지를 보정
+    # 이미지 전처리 - 침식, 팽창
     kernel = np.ones((5, 5), np.uint8)
     binary_frame = cv2.erode(binary_frame, kernel, iterations=1)
     binary_frame = cv2.dilate(binary_frame, kernel, iterations=1)
     
-    # 이진화된 이미지 표시
-    cv2.imshow('Binary Image', binary_frame)
+    cv2.imshow('Binary Image', binary_frame) # 이진화된 이미지 표시
     
-    # 노란색 홀컵 윤곽선 binary_frame
-    contours, _ = cv2.findContours(binary_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(binary_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # 노란색 홀컵 윤곽선
     
-    left_point, right_point = None, None
     
-    # 가장 왼쪽과 오른쪽 점 찾기
+    ##### 가장 왼쪽과 오른쪽 점 찾기
+    
+    left_point_y, right_point_y = None, None
+    
     if contours:
         for contour in contours:
             for point in contour:
                 x = point[0][0]
                 y = point[0][1]
 
-                # 가장 왼쪽에 있는 점 업데이트
-                if left_point is None or x < left_point[0]:
-                    left_point = [x, y]
+                # 가장 왼쪽점 업데이트
+                if left_point_y is None or x < left_point_y[0]:
+                    left_point_y = [x, y]
 
-                # 가장 오른쪽에 있는 점 업데이트
-                if right_point is None or x > right_point[0]:
-                    right_point = [x, y]
+                # 가장 오른쪽점 업데이트
+                if right_point_y is None or x > right_point_y[0]:
+                    right_point_y = [x, y]
 
-    # 결과 이미지에 표시
     result = frame.copy()
-    if left_point is not None:
-        cv2.circle(result, tuple(left_point), 5, (0, 0, 255), -1)
-    if right_point is not None:
-        cv2.circle(result, tuple(right_point), 5, (0, 0, 255), -1)
+    if left_point_y is not None:
+        cv2.circle(result, tuple(left_point_y), 5, (0, 0, 255), -1)
+    if right_point_y is not None:
+        cv2.circle(result, tuple(right_point_y), 5, (0, 0, 255), -1)
         
         
         
-    #################################################여기서부터 빨간공
-    
+    #############여기서부터 빨간공
     
     
     # 빨간공 인식
@@ -74,58 +72,56 @@ def detect_hole_in_golf_ball(frame):
     imgThresh = cv2.erode(imgThresh, np.ones((5, 5), np.uint8))
     imgThresh = cv2.dilate(imgThresh, np.ones((5, 5), np.uint8))
     
-    
     cv2.imshow('ball', imgThresh)
-        
-    # 빨간색 골프 공 윤곽선 찾기
-    contours, _ = cv2.findContours(imgThresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+         
+    contours, _ = cv2.findContours(imgThresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # 빨간색 골프 공 윤곽선
     
     
+    ##### 가장 왼쪽과 오른쪽 점 찾기
+
     left_point_red, right_point_red = None, None
     
-    # 가장 왼쪽과 오른쪽 점 찾기
     if contours:
         for contour in contours:
             for point in contour:
                 x_red = point[0][0]
                 y_red = point[0][1]
 
-                # 가장 왼쪽에 있는 점 업데이트
+                # 가장 왼쪽점 업데이트
                 if left_point_red is None or x_red < left_point_red[0]:
                     left_point_red = [x_red, y_red]
 
-                # 가장 오른쪽에 있는 점 업데이트
+                # 가장 오른쪽점 업데이트
                 if right_point_red is None or x_red > right_point_red[0]:
                     right_point_red = [x_red, y_red]
 
-    # 결과 이미지에 표시
-    if left_point is not None:
+    if left_point_red is not None:
         cv2.circle(result, tuple(left_point_red), 5, (0, 255, 0), -1)
-    if right_point is not None:
+    if right_point_red is not None:
         cv2.circle(result, tuple(right_point_red), 5, (0, 255, 0), -1)
         
         
-        
 
-    # 홀인 여부 판단
-    if left_point is not None and right_point is not None:
-        A, A_prime = left_point[0], right_point[0]
+    ##### 홀인 여부 판단
+    
+    if left_point_y is not None and right_point_y is not None:
+        A, A_prime = left_point_y[0], right_point_y[0]
         B, B_prime = left_point_red[0], right_point_red[0]
         
         if A < B < A_prime and A < B_prime < A_prime:
             hole_result = "HoleIn"
         else:
-            hole_result = "NO"
+            hole_result = "NO..."
     else:
-        hole_result = "NO"
+        hole_result = "NO..."
         
     return hole_result, result
 
-# 영상 파일 경로 설정
-video_path = "video/holein.avi"
+
+##### 메인
 
 # 영상 파일 열기
-cap = cv2.VideoCapture(video_path)
+cap = cv2.VideoCapture("HoleIn/video/holein.avi")
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -133,8 +129,7 @@ while cap.isOpened():
     if not ret:
         break
     
-    # 골프 홀 인식 수행
-    result, frame_with_points = detect_hole_in_golf_ball(frame)
+    result, frame_with_points = detect_hole_in(frame)
     print(result)
     
     cv2.imshow('Video', frame_with_points)
