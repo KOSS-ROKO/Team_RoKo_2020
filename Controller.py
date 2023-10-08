254ff818926477f7445aec75bf7d7e224a92411d
seo0o59 <seoyoung344@naver.com>
2023-10-08 18:11:56 +0900

    Feat: change controller file

--- Controller.py
+++ Controller.py
@@ -1,60 +1,30 @@
-from enum import Enum, auto
+# -*- coding: utf-8 -*-
 
 import time
-
-from Motion import Motion
 from Def.ImageProcessor import ImageProcessor
 from Robo import Robo
-
-#from Def.Detection import Detection
-
+from Motion import Motion
+from enum import Enum
 
 class Act(Enum):
-    # 0,1은 없음
-    TEESHOT = auto()  # 2. 맨 처음 티샷  
-    WALK_BALL = auto()  # 3. 공까지 걸어가기 (걸음수)
-    PUTTING_POS = auto()  # 4. 퍼팅 위치에 서기
-    PUTTING = auto()  # 5. 퍼팅
-    HOLEIN = auto() # 6. 홀인
-    
-
+    FIND_BALL = 1
 
 class Controller:
-    robo: Robo = Robo()
-    act: Act = Act.TEESHOT
-    
+    robo = Robo()
+    act  = Act.FIND_BALL
+
     @classmethod
-    def start(self):
+    def start(cls):
         # 정해진 파워로 한번 퍼팅.
-        act = self.act
-        robo: Robo = Robo()
-        
+        act = cls.act
+        robo = cls.robo
 
-        if act == act.TEESHOT:
+        if act == Act.FIND_BALL:
             print("ACT: ", act)  # Debug
-            
-            self._motion = Motion()  # Motion
-            # self._motion.set_head("DOWN", 30) # 티샷######################################
-            # time.sleep(0.5)
-            self.act = act.WALK_BALL
-        
-        
-        elif act == act.WALK_BALL:
-                        
-            state = Detection.detect_ball()
-            # state = self.robo._image_processor
-        
-            # findball 함수 호출
-            pass
+            state = robo._image_processor.detect_ball()
 
-        elif act == act.PUTTING_POS:
-            pass
-        elif act == act.PUTTING:
-            pass
-        
-        elif act == act.HOLEIN:
-            pass
-        
+            if state:
+                robo._motion.walk("FORWARD")
             
         
         
