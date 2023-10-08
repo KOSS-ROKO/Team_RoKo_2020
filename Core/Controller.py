d98686884797326d8091e8d2b9398db2191d66ec
HeeNamgoong <skrndgml2@kookmin.ac.kr>
2023-10-08 21:29:36 +0900

    Feat: Clear WALK_BALL

--- Core/Controller.py
+++ Core/Controller.py
@@ -25,6 +25,7 @@ class Controller:
         # 정해진 파워로 한번 퍼팅.
         act = self.act
         robo = self.robo
+        flag = 0
 
         if act == Act.TEESHOT:                 ##### 1. 시작 및 티샷
             print("ACT: ", act)  # Debug
@@ -32,31 +33,37 @@ class Controller:
             ################# 처음 티샷 모션 
             robo._motion.teeshot("")
             
+            # turn body left, 몸을 왼쪽으로 90도 돌림. / 고개는 이미 정면을 바라보고 있음.(바꿀 필요 없단 뜻)
+            robo._motion.body_left("")
+            
             act = Act.WALK_BALL
         
+        
         elif act == Act.WALK_BALL:             ##### 2. 공을 향해 걸어간다
-            check_ball = robo._image_processor.detect_ball()
+            if flag == 0: # 맨 처음만 실행
+                check_ball = robo._image_processor.detect_ball()
             
             if check_ball == True:
+                flag = 1 # 공 검출 완료
                 find_ball = Ball.middle_ball()
                 
-                while find_ball != "stop":
-                    if find_ball == "stop":
-                        act = Act.PUTTING_POS
-                    elif find_ball == "go right":
-                        robo._motion.head_right("") ################# 고개 오른쪽으로 돌리는 모션
-                        
-                        
-                        
-                    elif find_ball == "go left":
-                        robo._motion.head_left("") ################# 고개 왼쪽으로 돌리는 모션
+                
+                if find_ball == "stop":
+                    act = Act.PUTTING_POS
+                elif find_ball == "go right":
+                    robo._motion.head_right("") ################# 고개 오른쪽으로 돌리는 모션
                     
-                    elif find_ball == "go far":
-                        act = Act.WALK_BALL
+                elif find_ball == "go left":
+                    robo._motion.head_left("") ################# 고개 왼쪽으로 돌리는 모션
+                
+                elif find_ball == "go far":
+                    act = Act.WALK_BALL
                         
             else:
                 # 공이 화면에 안 보이는 경우
                 # 패닝 틸팅? or 걷기?
+                # 고개 각도 크게 돌리기, Find ball과 다름
+                flag = 0
                 pass
             
         elif act == Act.PUTTING_POS:             ##### 3. 퍼팅 위치에 서기
