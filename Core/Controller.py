3b93ac60d201d368f6b028df8a40e5e532c7280e
HeeNamgoong <skrndgml2@kookmin.ac.kr>
2023-10-08 19:35:30 +0900

    Feat: change code fit in rapi

--- Core/Controller.py
+++ Core/Controller.py
@@ -7,19 +7,24 @@ from Motion.Motion import Motion
 from enum import Enum
 
 class Act(Enum):
-    FIND_BALL = 1
+    
+    TEESHOT = 1          # 2. 맨 처음 티샷  
+    WALK_BALL = 2        # 3. 공까지 걸어가기 (걸음수)
+    PUTTING_POS = 3      # 4. 퍼팅 위치에 서기
+    PUTTING = 4          # 5. 퍼팅
+    OLEIN = 5            # 6. 홀인
 
 class Controller:
     robo = Robo()
-    act  = Act.FIND_BALL
+    act  = Act.TEESHOT
 
     @classmethod
-    def start(cls):
+    def start(self):
         # 정해진 파워로 한번 퍼팅.
-        act = cls.act
-        robo = cls.robo
+        act = self.act
+        robo = self.robo
 
-        if act == Act.FIND_BALL:
+        if act == Act.TEESHOT:
             print("ACT: ", act)  # Debug
             state = robo._image_processor.detect_ball()
 
