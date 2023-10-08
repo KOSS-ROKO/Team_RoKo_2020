993bfe3454d3dd64e1b4e01221b30fe030e30b97
HeeNamgoong <skrndgml2@kookmin.ac.kr>
2023-10-08 20:52:38 +0900

    Feat: copy and paste def

--- Vision/ImageProcessor.py
+++ Vision/ImageProcessor.py
@@ -403,3 +403,234 @@ class ImageProcessor:
         return (center_x, center_y)
         
         
+        
+        
+        
+        
+        #########################################
+        #########################################
+        ############## BALL MIDDLE ##############
+        #########################################
+        #########################################
+        
+        
+        
+    def middle_ball():
+        
+        origin = ImageProcessor.get_img()
+        frame = origin.copy()
+
+        # 화면을 11x11 그리드로 나누는 함수
+        def divide_screen(frame):
+            height, width = frame.shape[:2]
+            cell_height = height // 11
+            cell_width = width // 11
+            
+            # 그리드 라인 그리기
+            for i in range(1, 11):
+                cv2.line(frame, (0, i * cell_height), (width, i * cell_height), (0, 255, 0), 2)  # 녹색
+                cv2.line(frame, (i * cell_width, 0), (i * cell_width, height), (0, 255, 0), 2)
+            
+            return frame
+
+    
+        
+        # 빨간색 객체 추출
+        red_detected = ImageProcessor.detect_ball(frame)
+        
+        # 화면을 11x11 그리드로 나누고 그리드 라인 그리기
+        divided_frame = divide_screen(frame)
+        
+        #여기에 새로운 코드 추가
+        # 빨간색 객체 검출 및 위치 계산
+        non_zero_pixels = np.transpose(np.nonzero(red_detected))
+        
+        if non_zero_pixels.size > 0:
+            x_center = non_zero_pixels[:, 1].mean()
+            y_center = non_zero_pixels[:, 0].mean()
+            
+            cell_width = divided_frame.shape[1] // 11
+
+            # 빨간 공이 중앙 세로줄인 6번째 줄에서 검출되면 "stop" 출력
+            if (cell_width * 5 <= x_center <= cell_width * 6):
+                print("stop")
+                return "stop"
+            # 1~5번째 줄에서 검출되면 "go right" 출력
+            elif x_center < cell_width * 5:
+                print("go right")
+                return "go right"
+            # 7~11번째 줄에서 검출되면 "go left" 출력
+            elif x_center > cell_width * 6:
+                print("go left")
+                return "go left"
+        else:
+            print("go far")
+            return "go far"
+
+        
+        
+        
+        #########################################
+        #########################################
+        ############### ONEFRAME ################
+        #########################################
+        #########################################
+        
+        
+    def ball_hole_oneframe():
+        
+        origin = ImageProcessor.get_img()
+        frame = origin.copy()
+    
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+
+        lower_red = np.array([160, 70, 20])
+        upper_red = np.array([178, 255, 255])
+
+        red_mask = cv2.inRange(hsv_frame, lower_red, upper_red)
+        red_objects = cv2.bitwise_and(frame, frame, mask=red_mask)
+
+        lower_yellow = np.array([0, 80, 50])
+        upper_yellow = np.array([36, 250, 250])
+
+        yellow_mask = cv2.inRange(hsv_frame, lower_yellow, upper_yellow)
+        yellow_objects = cv2.bitwise_and(frame, frame, mask=yellow_mask)
+
+        # 노란색, 빨간색 객체 표시
+        #cv2.imshow('Yellow Objects', yellow_objects)
+        #cv2.imshow('Red Objects', red_objects)
+
+        y_contours, _ = cv2.findContours(yellow_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        r_contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+
+        # 노란색, 빨간색 물체의 크기를 확인할 최소 크기
+        min_yellow_size = 10000 #이 수치는 프레임 크기에 따라서 나중에 조정 필요할듯요 
+        min_red_size = 1000
+
+        # 노란색 물체를 찾았고 크기가 최소 크기 이상인지 검사
+        if y_contours and cv2.contourArea(max(y_contours, key=cv2.contourArea)) >= min_yellow_size \
+            and r_contours and cv2.contourArea(max(r_contours, key=cv2.contourArea)) >= min_red_size:
+            return True
+        else:
+            return False
+        
+        
+        
+        #########################################
+        #########################################
+        ############### HOLE IN #################
+        #########################################
+        #########################################
+        
+        
+    def detect_hole_in():
+        
+        origin = ImageProcessor.get_img()
+        frame = origin.copy()
+        
+        try:
+        
+            # 노란색 홀컵 검출
+            yellow_lower = np.array([20, 20, 100])
+            yellow_upper = np.array([36, 250, 250])
+            
+            hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            yellow_mask = cv2.inRange(hsv_frame, yellow_lower, yellow_upper)
+            yellow_objects = cv2.bitwise_and(frame, frame, mask=yellow_mask)
+            
+            #cv2.imshow('HoleCup', yellow_mask)
+            
+            blurred_frame = cv2.GaussianBlur(yellow_objects, (5, 5), 0)
+            gray_frame = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2GRAY)
+            
+            _, binary_frame = cv2.threshold(gray_frame, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+            
+            # 이미지 전처리 - 침식, 팽창
+            kernel = np.ones((5, 5), np.uint8)
+            binary_frame = cv2.erode(binary_frame, kernel, iterations=1)
+            binary_frame = cv2.dilate(binary_frame, kernel, iterations=1)
+            
+            cv2.imshow('Binary Image', binary_frame) # 이진화된 이미지 표시
+            
+            contours, _ = cv2.findContours(binary_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # 노란색 홀컵 윤곽선
+            
+            
+            ##### 가장 왼쪽과 오른쪽 점 찾기
+            
+            left_point_y, right_point_y = None, None
+            
+            if contours:
+                for contour in contours:
+                    for point in contour:
+                        x = point[0][0]
+                        y = point[0][1]
+
+                        # 가장 왼쪽점 업데이트
+                        if left_point_y is None or x < left_point_y[0]:
+                            left_point_y = [x, y]
+
+                        # 가장 오른쪽점 업데이트
+                        if right_point_y is None or x > right_point_y[0]:
+                            right_point_y = [x, y]
+
+            result = frame.copy()
+            if left_point_y is not None:
+                cv2.circle(result, tuple(left_point_y), 5, (0, 0, 255), -1)
+            if right_point_y is not None:
+                cv2.circle(result, tuple(right_point_y), 5, (0, 0, 255), -1)
+                
+                
+                
+            #############여기서부터 빨간공
+            
+            
+            # 빨간공 인식
+            imgThresh = ImageProcessor.detect_ball(frame)
+                
+            contours, _ = cv2.findContours(imgThresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # 빨간색 골프 공 윤곽선
+            
+            
+            ##### 가장 왼쪽과 오른쪽 점 찾기
+
+            left_point_red, right_point_red = None, None
+            
+            if contours:
+                for contour in contours:
+                    for point in contour:
+                        x_red = point[0][0]
+                        y_red = point[0][1]
+
+                        # 가장 왼쪽점 업데이트
+                        if left_point_red is None or x_red < left_point_red[0]:
+                            left_point_red = [x_red, y_red]
+
+                        # 가장 오른쪽점 업데이트
+                        if right_point_red is None or x_red > right_point_red[0]:
+                            right_point_red = [x_red, y_red]
+
+            if left_point_red is not None:
+                cv2.circle(result, tuple(left_point_red), 5, (0, 255, 0), -1)
+            if right_point_red is not None:
+                cv2.circle(result, tuple(right_point_red), 5, (0, 255, 0), -1)
+                
+                
+
+            ##### 홀인 여부 판단
+            if left_point_y is not None and right_point_y is not None:
+                A, A_prime = left_point_y[0], right_point_y[0]
+                B, B_prime = left_point_red[0], right_point_red[0]
+                
+                if A < B < A_prime and A < B_prime < A_prime:
+                    hole_result = "HoleIn"
+                else:
+                    hole_result = "NO..."
+            else:
+                hole_result = "NO..."
+                
+            return hole_result, result            
+        
+        except Exception as e:
+            print(f"Error: {e}")
+            return f"Error: {e}"
