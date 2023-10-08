784a7030105b29805d5cb10a034277761e4dce8c
HeeNamgoong <skrndgml2@kookmin.ac.kr>
2023-10-08 20:28:00 +0900

    Feat: create walk ball

--- Core/Controller.py
+++ Core/Controller.py
@@ -4,6 +4,7 @@ import time
 from Vision.ImageProcessor import ImageProcessor
 from Robo import Robo
 from Motion.Motion import Motion
+from Vision.Ball_middle import Ball
 
 
 class Act:
@@ -28,12 +29,38 @@ class Controller:
         act = self.act
         robo = self.robo
 
-        if act == Act.TEESHOT:
+        if act == Act.TEESHOT:                  # 
             print("ACT: ", act)  # Debug
-            state = robo._image_processor.detect_ball()
-
-            if state:
-                robo._motion.walk("FORWARD")
             
+            ################# 처음 티샷 모션 
+            robo._motion.teeshot("")
+            
+            act = Act.WALK_BALL
+            
+            #state = robo._image_processor.detect_ball()
         
+        elif act == Act.WALK_BALL:               #
+            check_ball = robo._image_processor.detect_ball()
+            
+            if check_ball == True:
+                find_ball = Ball.middle_ball()
+                
+                while find_ball != "stop":
+                    if find_ball == "stop":
+                        act = Act.PUTTING_POS
+                    elif find_ball == "go right":
+                        robo._motion.head_right("") ################# 고개 오른쪽으로 돌리는 모션
+                        
+                        
+                        
+                    elif find_ball == "go left":
+                        robo._motion.head_left("") ################# 고개 왼쪽으로 돌리는 모션
+                    
+                    elif find_ball == "go far":
+                        act = Act.WALK_BALL
+                        
+            else:
+                # 공이 화면에 안 보이는 경우
+                # 패닝 틸팅? or 걷기?
+                pass
         
