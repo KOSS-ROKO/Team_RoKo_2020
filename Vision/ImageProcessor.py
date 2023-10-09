11521f9878ae86562732271766ba682bda9f9a8e
seo0o59 <seoyoung344@naver.com>
2023-10-09 17:28:55 +0900

    Feat: Add y coordinate to accurate hole in

--- Vision/ImageProcessor.py
+++ Vision/ImageProcessor.py
@@ -506,7 +506,7 @@ class ImageProcessor:
 
 
         # 노란색, 빨간색 물체의 크기를 확인할 최소 크기
-        min_yellow_size = 10000 #이 수치는 프레임 크기에 따라서 나중에 조정 필요할듯요 
+        min_yellow_size = 10000 # 이 수치는 프레임 크기에 따라서 나중에 조정 필요할듯요 
         min_red_size = 1000
 
         # 노란색 물체를 찾았고 크기가 최소 크기 이상인지 검사
@@ -531,7 +531,6 @@ class ImageProcessor:
         frame = origin.copy()
         
         try:
-        
             # 노란색 홀컵 검출
             yellow_lower = np.array([20, 20, 100])
             yellow_upper = np.array([36, 250, 250])
@@ -539,8 +538,6 @@ class ImageProcessor:
             hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             yellow_mask = cv2.inRange(hsv_frame, yellow_lower, yellow_upper)
             yellow_objects = cv2.bitwise_and(frame, frame, mask=yellow_mask)
-            
-            #cv2.imshow('HoleCup', yellow_mask)
             
             blurred_frame = cv2.GaussianBlur(yellow_objects, (5, 5), 0)
             gray_frame = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2GRAY)
@@ -552,14 +549,15 @@ class ImageProcessor:
             binary_frame = cv2.erode(binary_frame, kernel, iterations=1)
             binary_frame = cv2.dilate(binary_frame, kernel, iterations=1)
             
-            cv2.imshow('Binary Image', binary_frame) # 이진화된 이미지 표시
+            cv2.imshow('Yellow Holecup Binary Image', binary_frame) # 이진화된 이미지 표시
             
             contours, _ = cv2.findContours(binary_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # 노란색 홀컵 윤곽선
             
             
-            ##### 가장 왼쪽과 오른쪽 점 찾기
+            ##### 가장 왼쪽과 오른쪽, 위쪽 아래쪽 점 찾기
             
             left_point_y, right_point_y = None, None
+            up_point_yellow, down_point_yellow = None, None
             
             if contours:
                 for contour in contours:
@@ -568,18 +566,36 @@ class ImageProcessor:
                         y = point[0][1]
 
                         # 가장 왼쪽점 업데이트
-                        if left_point_y is None or x < left_point_y[0]:
-                            left_point_y = [x, y]
+                        if left_point_yellow is None or x < left_point_yellow[0]:
+                            left_point_yellow = [x, y]
 
                         # 가장 오른쪽점 업데이트
-                        if right_point_y is None or x > right_point_y[0]:
-                            right_point_y = [x, y]
+                        if right_point_yellow is None or x > right_point_yellow[0]:
+                            right_point_yellow = [x, y]
+
+                        # 가장 아래점 업데이트
+                        if down_point_yellow is None or y > down_point_yellow[1]:
+                            down_point_yellow = [x, y]
+
+                        diameter = 0
+                        # 가장 위쪽점 업데이트
+                        if down_point_yellow is not None:
+                            diameter = 2 * abs(left_point_yellow[1] - down_point_yellow[1]) - 20
+
+                        # 제일 위쪽 점 계산
+                        up_point_yellow = [down_point_yellow[0], down_point_yellow[1] - diameter]
+
 
             result = frame.copy()
-            if left_point_y is not None:
-                cv2.circle(result, tuple(left_point_y), 5, (0, 0, 255), -1)
-            if right_point_y is not None:
-                cv2.circle(result, tuple(right_point_y), 5, (0, 0, 255), -1)
+            if left_point_yellow is not None:
+                cv2.circle(result, tuple(left_point_yellow), 5, (0, 0, 255), -1)
+            if right_point_yellow is not None:
+                cv2.circle(result, tuple(right_point_yellow), 5, (0, 0, 255), -1)
+
+            if up_point_yellow is not None:
+                cv2.circle(result, tuple(up_point_yellow), 5, (0, 0, 255), -1)
+            if down_point_yellow is not None:
+                cv2.circle(result, tuple(down_point_yellow), 5, (0, 0, 255), -1)
                 
                 
                 
@@ -588,13 +604,16 @@ class ImageProcessor:
             
             # 빨간공 인식
             imgThresh = ImageProcessor.detect_ball(frame)
+
+            cv2.imshow('Red Ball Binary Image', imgThresh) # 이진화된 이미지 표시
                 
             contours, _ = cv2.findContours(imgThresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # 빨간색 골프 공 윤곽선
             
             
-            ##### 가장 왼쪽과 오른쪽 점 찾기
+            ##### 가장 왼쪽과 오른쪽, 위쪽과 아래쪽 점 찾기
 
             left_point_red, right_point_red = None, None
+            up_point_red, down_point_red = None, None
             
             if contours:
                 for contour in contours:
@@ -610,19 +629,36 @@ class ImageProcessor:
                         if right_point_red is None or x_red > right_point_red[0]:
                             right_point_red = [x_red, y_red]
 
+                        # 가장 위쪽점 업데이트
+                        if up_point_red is None or y_red < up_point_red[1]:
+                            up_point_red = [x_red, y_red]
+
+                        # 가장 아래쪽점 업데이트
+                        if down_point_red is None or y_red > down_point_red[1]:
+                            down_point_red = [x_red, y_red]
+
+
             if left_point_red is not None:
                 cv2.circle(result, tuple(left_point_red), 5, (0, 255, 0), -1)
             if right_point_red is not None:
                 cv2.circle(result, tuple(right_point_red), 5, (0, 255, 0), -1)
                 
-                
+            if up_point_red is not None:
+                cv2.circle(result, tuple(up_point_red), 5, (0, 255, 0), -1)
+            if down_point_red is not None:
+                cv2.circle(result, tuple(down_point_red), 5, (0, 255, 0), -1)  
+
+
 
             ##### 홀인 여부 판단
             if left_point_y is not None and right_point_y is not None:
                 A, A_prime = left_point_y[0], right_point_y[0]
                 B, B_prime = left_point_red[0], right_point_red[0]
+
+                C, C_prime = left_point_yellow[1], right_point_yellow[1]
+                D, D_prime = left_point_red[1], right_point_red[1]
                 
-                if A < B < A_prime and A < B_prime < A_prime:
+                if A < B < A_prime and A < B_prime < A_prime and C < D < C_prime and C < D_prime < C_prime:
                     hole_result = True
                 else:
                     hole_result = False
@@ -632,5 +668,4 @@ class ImageProcessor:
             return hole_result        
         
         except Exception as e:
-            print(f"Error: {e}")
-            return f"Error: {e}"
+            return "Error"
