55788d4f218ae6ea0be276c27b21f1e13d96155e
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2023-11-04 12:13:16 +0900

    Feat: Add global var init

--- Core/variable.py
+++ Core/variable.py
@@ -3,6 +3,7 @@
 global _Head_UD_Middle_Value
 global _Head_Length_list
 global Head_UD_Middle_Value_Measures
+Head_UD_Middle_Value_Measures = 100 # 이거 뭔데? 수정 안 해도 됨?
 global Head_ud_angle
 global Length_Weight
 global Length_ServoAngle
