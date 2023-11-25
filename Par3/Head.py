938a1f8451465c12ac28c64f5efd8f9c8d0e41e2
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2023-11-25 15:18:32 +0900

    Refactor: Straight turn angle

--- Par3/Head.py
+++ Par3/Head.py
@@ -317,13 +317,13 @@ class Head:
         elif check == "right":   ### 찌그째그 걸음으로 오른쪽으로 원그리며 이동
             robo._motion.walk_side("RIGHT10")
             time.sleep(0.5)
-            robo._motion.turn("LEFT", 10)    # 값 조절 필요
+            robo._motion.turn("LEFT", 15)    # 값 조절 필요
             time.sleep(1)
             return False               
         elif check == "left":    ## 똑같이 왼쪽으로 이동
             robo._motion.walk_side("LEFT10")
             time.sleep(0.5)
-            robo._motion.turn("RIGHT", 10)    # 값 조절 필요
+            robo._motion.turn("RIGHT", 15)    # 값 조절 필요
             time.sleep(1)
             return False
         else: # check == "none"
