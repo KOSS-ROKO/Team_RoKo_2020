df29cf3c8f44ea4b966512458701f90e3562fc1e
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2023-12-04 13:45:11 +0900

    Color_Red.py

--- Pre Setting/Color_Red.py
+++ Pre Setting/Color_Red.py
@@ -5,13 +5,9 @@ import numpy as np
 mouse_x, mouse_y = 0, 0
 
 # 마지막으로 저장한 트랙바 값 초기화
-last_values_red1 = {
-    'low_h': 0, 'high_h': 17, 'low_s': 80, 'high_s': 120, 'low_v': 210, 'high_v': 255,
-}
+last_values_red1 = { 'low_h': 0, 'high_h': 10, 'low_s': 40, 'high_s': 155, 'low_v': 120, 'high_v': 255, }
 
-last_values_red2 = {
-    'low_h': 0, 'high_h': 17, 'low_s': 80, 'high_s': 120, 'low_v': 210, 'high_v': 255,
-}
+last_values_red2 = { 'low_h': 160, 'high_h': 180, 'low_s': 50, 'high_s': 255, 'low_v': 100, 'high_v': 255, }
 
 # 마우스 이벤트 콜백 함수 정의
 def mouse_callback(event, x, y, flags, param):
@@ -27,18 +23,19 @@ cap.set(4, 480)
 cap.set(5, 5)
 
 # 윈도우 생성
-cv2.namedWindow('Trackbar', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Trackbar', 500, 400)
+cv2.namedWindow('Trackbar1', cv2.WINDOW_NORMAL)
+cv2.namedWindow('Trackbar2', cv2.WINDOW_NORMAL)
 
 # 마우스 이벤트 콜백 함수 등록
-cv2.setMouseCallback('Trackbar', mouse_callback)
+cv2.setMouseCallback('Trackbar1', mouse_callback)
+cv2.setMouseCallback('Trackbar2', mouse_callback)
 
 # 트랙바 초기화
 for name, value in last_values_red1.items():
-    cv2.createTrackbar(f'trackbar1_{name}', 'Trackbar', value, 255, lambda x: None)
+    cv2.createTrackbar(f'trackbar1_{name}', 'Trackbar1', value, 255, lambda x: None)
 
 for name, value in last_values_red2.items():
-    cv2.createTrackbar(f'trackbar2_{name}', 'Trackbar', value, 255, lambda x: None)
+    cv2.createTrackbar(f'trackbar2_{name}', 'Trackbar2', value, 255, lambda x: None)
 
 while True:
     # 프레임 읽기
@@ -50,10 +47,10 @@ while True:
 
     # 트랙바 값 얻기
     for name in last_values_red1.keys():
-        last_values_red1[name] = cv2.getTrackbarPos(f'trackbar1_{name}', 'Trackbar')
+        last_values_red1[name] = cv2.getTrackbarPos(f'trackbar1_{name}', 'Trackbar1')
 
     for name in last_values_red2.keys():
-        last_values_red2[name] = cv2.getTrackbarPos(f'trackbar2_{name}', 'Trackbar')
+        last_values_red2[name] = cv2.getTrackbarPos(f'trackbar2_{name}', 'Trackbar2')
 
     # 빨간색 공 인식
     imgHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -82,12 +79,14 @@ while True:
         cv2.putText(frame, f'HSV: {color_hsv}', (mouse_x, mouse_y + 50),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
 
-    cv2.imshow('Trackbar', frame)
+    cv2.imshow('Trackbar1', frame)
+    cv2.imshow('Trackbar2', frame)
+    cv2.imshow('Real Window', frame)
     cv2.imshow('Red1', red_detected1)
 
     # 창의 위치 조정
-    cv2.moveWindow('Real Window', 600, 0)
-    cv2.moveWindow('Red1', 600, 500)
+    #cv2.moveWindow('Real Window', 600, 0)
+    #cv2.moveWindow('Red1', 600, 500)
 
     # 'q' 키를 누르면 종료
     key = cv2.waitKey(1) & 0xFF
