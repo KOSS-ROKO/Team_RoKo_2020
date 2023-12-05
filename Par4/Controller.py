2264baad86e0bc02f4189116e0f2170a173683e4
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2023-12-06 02:20:53 +0900

    Test: test

--- Par4/Controller.py
+++ Par4/Controller.py
@@ -696,7 +696,7 @@ class Controller:
 
             print("^^두번째 티샷^^")
             
-            """
+            
 
 
             motion.head("UP", 9)
@@ -765,7 +765,7 @@ class Controller:
             else :      # 최소 거리 18보다 더 가까이 있을 경우: 뒷걸음질
                 motion.walk("BACKWARD", ball_dist - 18)
                 time.sleep(2)
-            """
+            
             time.sleep(1)
             ball_pos('pr')
             time.sleep(1)
